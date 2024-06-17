2329d2dcc04ed5514949d48c8d9d383e736d08ac
DESKTOP-6N9G5LS\USUARIO <aarroyaveduque20@gmail.com>
2024-06-17 11:35:54 -0500

    Visualización de información de las páginas por medio de handles (hbs)

--- webserver/app.js
+++ webserver/app.js
@@ -1,16 +1,33 @@
 import express from 'express'
+import hbs from 'hbs'
 import path from 'path'
 
 const app = express()
 const PORT = 8080
 const __dirname = path.resolve()
+const information = {
+    name: 'TEMPLATED',
+    title: 'Road Trip'
+}
+
+// Handlebar
+app.set('view engine', 'hbs')
+hbs.registerPartials(`${__dirname}/views/partials`)
 
 // Servir contenido estático
 // Se debe especificar el path o ruta 'public'
 app.use(express.static('public'))
 
-app.get('/login', function (req, res) {
-    res.send('Hello from the login page')
+app.get('/', function (req, res) {
+    res.render('home', information)
+})
+
+app.get('/generic', function (req, res) {
+    res.render('generic', information)
+})
+
+app.get('/elements', function (req, res) {
+    res.render('elements', information)
 })
 
 app.get('*', function (req, res) {
